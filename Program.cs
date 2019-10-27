a21d382c4d967b11f0e058199157b847238ad134
Matt Obert on Windows <matt@hotwebmatter.com>
2019-10-27 18:31:30 -0400

    Continue debugging with more sample input

--- Program.cs
+++ Program.cs
@@ -9,6 +9,12 @@ namespace BonusApp
         {
             Bonus aBonus = new Bonus("Rosa Caili", 1500000, '2');
             WriteLine(aBonus.ToString());
+            Bonus bBonus = new Bonus("David Ecco", 600000, '4');
+            WriteLine(bBonus.ToString());
+            Bonus cBonus = new Bonus("Paul Gorman", 0, '2');
+            WriteLine(cBonus.ToString());
+            Bonus dBonus = new Bonus("Ruth Basso", 40000, '7');
+            WriteLine(dBonus.ToString());
             ReadLine();
         }
     }
