3ee22debd0ffa96d4c29cb707b0164b2f2c1430f
Matt Obert on Windows <matt@hotwebmatter.com>
2019-10-27 16:50:34 -0400

    Stub out Bonus properties, constructors, setters

--- Bonus.cs
+++ Bonus.cs
@@ -6,5 +6,36 @@ namespace BonusApp
 {
     class Bonus
     {
+        // private properties
+        string salespersonName;
+        double salesAmount;
+        char salesCode;
+
+        public Bonus()
+        {
+            // constructor without arguments
+        }
+
+        public Bonus(string name, double sales, char code)
+        {
+            // overloaded constructor with arguments
+            salespersonName = name;
+            salesAmount = sales;
+            salesCode = code;
+        }
+
+        // property mutator methods (setters)
+        public string SalespersonName
+        {
+            set { salespersonName = value; }
+        }
+        public double SalesAmount
+        {
+            set { salesAmount = value; }
+        }
+        public char SalesCode
+        {
+            set { salesCode = value; }
+        }
     }
 }
